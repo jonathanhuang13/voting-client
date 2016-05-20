9f05f7c12435cb2e11fbd51de4659c550e3f0ef8
Jonathan Huang <jonathanhuang@secondspectrum.com>
2016-05-20 14:35:33 -0700

    Using PureRenderMixin

--- src/components/voting.jsx
+++ src/components/voting.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import Winner from './winner'
-import Vote from './vote'
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './winner';
+import Vote from './vote';
 
 export default React.createClass({
+  mixins: [ PureRenderMixin ],
+  
   render: function() {
     if(this.props.winner){
       return (
