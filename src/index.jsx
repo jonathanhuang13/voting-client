6af21c1ee7db4fa35fed3b2ccbabbedddf3ded3f
Jonathan Huang <jonathanhuang@secondspectrum.com>
2016-05-20 15:02:04 -0700

    Route results page

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route } from 'react-router';
+import { Route, Router, hashHistory } from 'react-router';
 import App from './components/app';
 import Voting from './components/voting';
+import Results from './components/results';
 
 const pair = [ 'V for Vendetta', 'Batman' ];
-const routes = <Route component={App}>
-  <Route path='/' component={Voting} />
-</Route>
 
 ReactDOM.render(
-  <Router history={ hashHistory }>{routes}</Router>,
+  <Router history={ hashHistory }>
+    <Route component={App}>
+      <Route path='/' component={Voting} />
+      <Route path='/results' component={Results} />
+    </Route>
+  </Router>,
+
   document.getElementById('app')
 );
