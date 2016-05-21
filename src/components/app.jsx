e8f4c1151e84072a6abdb270098c78bf13ace001
Jonathan Huang <jonathanhuang@secondspectrum.com>
2016-05-20 18:13:50 -0700

    inc on results page

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
-const pair = List.of('V for Vendetta', 'Batman');
+const pair  = List.of('V for Vendetta', 'Batman');
+const tally = Map({ 'V for Vendetta': 5, 'Batman': 3 });
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, { pair: pair });
+    return React.cloneElement(this.props.children, { pair: pair, tally: tally });
   }
 });
--- src/components/results.jsx
+++ src/components/results.jsx
@@ -4,7 +4,42 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 export default React.createClass({
   mixins: [ PureRenderMixin ],
 
+  getPair: function() {
+    return this.props.pair || [];
+  },
+
+  getVotes: function(entry) {
+    if(!this.props.tally)
+      return 0
+
+    const tally = this.props.tally.get(entry);
+
+    if(tally)
+      return tally;
+    else
+      return 0;
+  },
+  
+  renderPairs: function() {
+    return this.getPair().map(this.renderPair);
+  },
+
+  renderPair: function(entry) {
+    return(
+      <div key={entry} className='entry'>
+        <h1>{entry}</h1>
+        <div className='voteCount'>
+          {this.getVotes(entry)}
+        </div>
+      </div>
+    );
+  },
+
   render: function() {
-    return <div>Hello!</div>
+    return (
+      <div className='results'>
+        {this.renderPairs()}
+      </div>
+    );
   }
 });
