78453c800f0be45cb4d1090bfc2d9a3f84e9612d
Jonathan Huang <jonathanhuang@secondspectrum.com>
2016-06-07 09:20:21 -0700

    setup socket connection

--- src/index.jsx
+++ src/index.jsx
@@ -3,21 +3,18 @@ import ReactDOM from 'react-dom';
 import { Route, Router, hashHistory } from 'react-router';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/app';
 import { VotingContainer } from './components/voting';
 import { ResultsContainer } from './components/results';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: [ 'V for Vendetta', 'Batman' ],
-      tally: { 'V for Vendetta': 2 }
-    }
-  }
-});
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+
+socket.on('state', state =>
+  store.dispatch({ type: 'SET_STATE', state })
+);
 
 ReactDOM.render(
   <Provider store={store}>
