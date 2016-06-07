1fa4b6d5ae604471efddd65d987a3bfab2974f81
Jonathan Huang <jonathanhuang@secondspectrum.com>
2016-06-07 09:08:10 -0700

    cleanup app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import { List, Map } from 'immutable';
-
-const pair  = List.of('V for Vendetta', 'Batman');
-const tally = Map({ 'V for Vendetta': 5, 'Batman': 3 });
-const winner = 'V For Vendetta';
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, { winner: winner, pair: pair, tally: tally });
+    return this.props.children;
   }
 });
